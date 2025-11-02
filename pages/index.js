3fc69e17f5ae667139fb135863497d2b7b9ccc7d
Lisandra Lopes <53584535+lisandralopesroque@users.noreply.github.com>
2025-11-02 23:57:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Luiz, eu te amo daqui até a eternidade! ❤❤ </h1>
-
+function Home() {
+  return <h1>Luiz, eu te amo daqui até a eternidade! ❤❤ </h1>;
 }
 
 export default Home;
